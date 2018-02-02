f7ff1069130be1496822872c2cf8e512baabe0af
Cesar Amaro <cesar.amaro@20d.mx>
2018-02-02 16:46:07 -0600

    api key plus formater phone number

--- api.php
+++ api.php
@@ -1,52 +1,63 @@
 <?php
+	
+	$api_key = 'DK2H30D27C';
 
-	if (isset($_REQUEST['phone'])) {
-		require 'Chargebee/lib/ChargeBee.php';
+	if (isset($_REQUEST['api_key'])) {
+		if ($_REQUEST['api_key'] == $api_key) {
+			if (isset($_REQUEST['phone'])) {
+				require 'Chargebee/lib/ChargeBee.php';
 
-		ChargeBee_Environment::configure("vatoday-test","test_Gk4YTcKidSlp09GjYCGemgHUW5u3l7Yf");
-		$c;
-		$mssg = 'Client found!';
-		$success = true;
-		$client_sent = array();
-		$customers = ChargeBee_Customer::all(
-			array(
-				"limit" => 1, 
-				"phone[is]" => $_REQUEST['phone'],
-				"sortBy[desc]" => "created_at",
-			)
-		);
-		if (count($customers) > 0) {
-			foreach ($customers as $customer){
-				$c = $customer->customer();
-				$client_sent['name'] = $c->firstName.' '.$c->lastName;
-				$client_sent['email'] = $c->email;
-				$client_sent['phone'] = $c->phone;
-				$subs = ChargeBee_Subscription::all(
+				ChargeBee_Environment::configure("vatoday-test","test_Gk4YTcKidSlp09GjYCGemgHUW5u3l7Yf");
+				$c;
+				$number = substr($_REQUEST['phone'], 2);
+				$mssg = 'Client found!';
+				$success = true;
+				$client_sent = array();
+				$customers = ChargeBee_Customer::all(
 					array(
 						"limit" => 1, 
-						"customerId[is]" => $c->id,
-						"status[is]" => 'active',
+						"phone[is]" => $number,
+						"sortBy[desc]" => "created_at",
 					)
 				);
-				if (count($subs) > 0) {
-					foreach ($subs as $sub) {
-						$sub = $sub->subscription();
-						$success = true;
+				if (count($customers) > 0) {
+					foreach ($customers as $customer){
+						$c = $customer->customer();
+						$client_sent['name'] = $c->firstName.' '.$c->lastName;
+						$client_sent['email'] = $c->email;
+						$client_sent['phone'] = $c->phone;
+						$subs = ChargeBee_Subscription::all(
+							array(
+								"limit" => 1, 
+								"customerId[is]" => $c->id,
+								"status[is]" => 'active',
+							)
+						);
+						if (count($subs) > 0) {
+							foreach ($subs as $sub) {
+								$sub = $sub->subscription();
+								$success = true;
+							}
+						} else {
+							$mssg = 'The client does not have an active account on ChargeBee.';
+							$success = false;
+						}
 					}
 				} else {
-					$mssg = 'The client does not have an active account on ChargeBee.';
+					$mssg = 'There is no client with that phone number registered on ChargeBee.';
 					$success = false;
 				}
+
+				$data = [
+					'mssg' => $mssg,
+					'success' => $success,
+					'customer' => $client_sent,
+				];
 			}
 		} else {
-			$mssg = 'There is no client with that phone number registered on ChargeBee.';
-			$success = false;
+			$data = ['mssg' => 'API key is incorrect.'];
 		}
-
-		$data = [
-			'mssg' => $mssg,
-			'success' => $success,
-			'customer' => $client_sent,
-		];
-		echo (json_encode($data)); exit();
+	} else {
+		$data = ['mssg' => 'No API key given.'];
 	}
+	echo (json_encode($data)); exit();
